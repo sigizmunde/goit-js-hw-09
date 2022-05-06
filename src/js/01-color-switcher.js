b2337cec7ff406c1fbe7ff841826492dfb4d8728
illya bondar <1816579@gmail.com>
2022-05-07 02:21:58 +0300

    first done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    startBtn: document.querySelector("[data-start]"),
+    stopBtn: document.querySelector("[data-stop]"),
+}
 
+let timerId = 0;
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener("click", onStart);
+refs.stopBtn.addEventListener("click", onStop);
+
+function onStart() {
+    timerId = setInterval(() => changeBodyColor(getRandomHexColor), 1000);
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+}
+
+function onStop() {
+    clearInterval(timerId);
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+}
+
+function changeBodyColor(colorFunction) {
+    document.body.style.backgroundColor = colorFunction();    
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0")}`;
+}
