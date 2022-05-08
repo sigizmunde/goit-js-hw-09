e972049750e4b0559fa31ae074f2a3186b89eaa8
illya bondar <1816579@gmail.com>
2022-05-08 17:32:30 +0300

    second fixed

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,7 +4,6 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import 'notiflix';
 
 Notify.init({
   width: '280px',
